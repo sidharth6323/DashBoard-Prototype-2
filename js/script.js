bd01597395eb6425ff354452418099d330047ab9
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-06-22 03:43:52 +0530

    First Day, Last Commit

--- js/script.js
+++ js/script.js
@@ -8,24 +8,32 @@ $(document).ready(function(){
 
 function clickNewsFeed(){
 
-		if(currentModule!="newsfeed"){
+		//if(currentModule!="newsfeeds"){
 
 			if(currentModule==""){
-				$( "#canvas" ).load( "modules/newsfeed.html" ).addClass("animated fadeInLeft")
+				$( "#canvas" ).load( "modules/newsfeed.html" )
+				$( "#article-container" ).addClass("animated fadeInLeft");
+				$( "#article-material" ).addClass("animated fadeInLeft");
+
 			}else{
 
-			$( " #canvas " ).removeClass("fadeInLeft").addClass( "fadeOutRight" );
-		console.log("clickNewsFeed");
+			$( "#article-container" ).removeClass("animated fadeInLeft").addClass( "fadeOutDown" );
+			$( "#article-material" ).removeClass("animated fadeInLeft").addClass( "fadeOutDown" );
+
 
 		setTimeout(function(){		  
 			$( "#canvas" ).load( "modules/newsfeed.html" );
-			$( "#canvas" ).removeClass(" fadeOutRight hide").addClass("fadeInLeft");
+
+			$( "#article-container" ).removeClass(" fadeOutDown hide").addClass("fadeInLeft");
+			$( "#article-material" ).removeClass(" fadeOutDown hide").addClass("fadeInLeft");
+
 			currentModule = "newsfeed";
-		
+			console.log("clickNewsFeed  " + currentModule);
+			
 		}, 500);	
     	}
 
-		}
+	//	}
     	
     };
 
@@ -35,12 +43,13 @@ function clickSoftware(){
 
 			$( " #canvas " ).removeClass("fadeInLeft").addClass( "fadeOutRight" );
 			
-			console.log("clickNewsFeed");
 
 		setTimeout(function(){		  
 			$( "#canvas" ).load( "modules/newsfeed.html" );
 			$( "#canvas" ).removeClass(" fadeOutRight hide").addClass("fadeInLeft");
+
 			currentModule = "softwares";
+			console.log("clickSoftware  " + currentModule);
 		
 		}, 500);	
     	}
